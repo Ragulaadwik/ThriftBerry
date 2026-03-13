36daaef39c9dcf1fd9d6f1f9cd4cddc3afb94b41
Ragulaadwik <aadwikrahul@gmail.com>
2026-03-13 15:36:16 +0530

    Initial microservice project structure

---
+++ ProductService/src/test/java/com/thriftBerry/ProductService/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thriftBerry.ProductService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
